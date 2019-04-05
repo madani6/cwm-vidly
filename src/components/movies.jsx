149afb42f8f5ea5d61ed35afdf9a56494cdc4e48
madani6 <anandamadani06@gmail.com>
2019-04-05 14:36:01 +0700

    pagination - handling page changes

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -7,6 +7,7 @@ import Pagination from "./common/pagination";
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    currentPage: 1,
     pageSize: 4
   };
 
@@ -24,11 +25,12 @@ class Movies extends Component {
   };
 
   handlePageChange = page => {
-    console.log(page);
+    this.setState({ currentPage: page });
   };
 
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage } = this.state;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
 
@@ -74,7 +76,8 @@ class Movies extends Component {
         </table>
         <Pagination
           itemCount={count}
-          pageSize={10}
+          pageSize={pageSize}
+          currentPage={currentPage}
           onPageChange={this.handlePageChange}
         />
       </React.Fragment>
