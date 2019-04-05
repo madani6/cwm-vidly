84214ec747a95ff323c8fdb3798bd8eefa61da18
madani6 <anandamadani06@gmail.com>
2019-04-05 09:41:57 +0700

    building the movies component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Movies from "./components/movies";
 import "./App.css";
 
 class App extends Component {
@@ -6,7 +7,7 @@ class App extends Component {
     return (
       <div>
         <main className="container">
-          <h1>Hello</h1>
+          <Movies />
         </main>
       </div>
     );
