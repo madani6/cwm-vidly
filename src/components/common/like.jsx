f70388092a26e6f45c204401bc828adc8cc08e2d
madani6 <anandamadani06@gmail.com>
2019-04-05 10:59:18 +0700

    composing end

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,21 +1,19 @@
 import React, { Component } from "react";
 
-const Like = () => {
-  let classes = "fa fa-heart";
-  if (!this.props.liked) classes += "-o";
-  return (
-    <i
-      onClick={this.props.onClick}
-      style={{ cursor: "pointer" }}
-      className={classes}
-      aria-hidden="true"
-    />
-  );
-};
-
 class Like extends Component {
   render() {
-    return <div />;
+    let classes = "fa fa-heart";
+    if (!this.props.liked) classes += "-o";
+    return (
+      <div>
+        <i
+          onClick={this.props.onClick}
+          style={{ cursor: "pointer" }}
+          className={classes}
+          aria-hidden="true"
+        />
+      </div>
+    );
   }
 }
 
