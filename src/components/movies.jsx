ecc3e621d680333199d0c91f6ec689991138446a
madani6 <anandamadani06@gmail.com>
2019-04-06 18:13:59 +0700

    filtering - implementing filtering

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -41,11 +41,19 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, movies: allMovies } = this.state;
+    const {
+      pageSize,
+      currentPage,
+      selectedGenre,
+      movies: allMovies
+    } = this.state;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
 
-    const movies = paginate(allMovies, currentPage, pageSize);
+    const filtered = selectedGenre
+      ? allMovies.filter(m => m.genre._id === selectedGenre._id)
+      : allMovies;
+    const movies = paginate(filtered, currentPage, pageSize);
 
     return (
       <div className="row">
@@ -57,7 +65,7 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <p>Showing {this.state.movies.length} movies in the database.</p>
+          <p>Showing {filtered.length} movies in the database.</p>
 
           <table className="table">
             <thead>
@@ -96,7 +104,7 @@ class Movies extends Component {
             </tbody>
           </table>
           <Pagination
-            itemCount={count}
+            itemCount={filtered.length}
             pageSize={pageSize}
             currentPage={currentPage}
             onPageChange={this.handlePageChange}
